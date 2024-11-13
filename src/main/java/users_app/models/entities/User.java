977f5c10d14072d71190c759242446bc04f2ae84
patinodeveloper <pepe_toni_02@hotmail.com>
2024-11-13 13:09:15 -0600

    feat(UserEntity): class User created in models.entities
    feat(UserRepository): class UserRepository created in repositories

---
+++ src/main/java/users_app/models/entities/User.java
@@ -0,0 +1,28 @@
+package users_app.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+}
---
+++ src/main/java/users_app/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package users_app.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import users_app.models.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    //
+}
