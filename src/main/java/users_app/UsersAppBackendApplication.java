6acafac4248041b67110a71cb524b609d1f02459
patinodeveloper <pepe_toni_02@hotmail.com>
2024-11-13 12:57:07 -0600

    Initial Commit: project created

---
+++ src/main/java/users_app/UsersAppBackendApplication.java
@@ -0,0 +1,13 @@
+package users_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersAppBackendApplication.class, args);
+	}
+
+}
